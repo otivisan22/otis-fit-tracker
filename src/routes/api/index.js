c7e753e90b03bb1d1886e15e0a0e5cbaa924f113
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-07-13 00:39:46 +0100

    refactored html route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,1 +1,20 @@
+const { Router } = require("express");
 
+const {
+  getWorkouts,
+  createWorkout,
+  updateWorkout,
+  getWorkoutRange,
+} = require("../../controllers/api/workouts");
+
+const router = Router();
+
+router.get("/", getWorkouts);
+
+router.post("/", createWorkout);
+
+router.put("/:id", updateWorkout);
+
+router.get("/", getWorkoutRange);
+
+module.exports = router;
