da9c87955a694ccffb57583360475f4902049382
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-07-13 18:16:39 +0100

    workout model

---
+++ src/models/workout.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Type of exercise is required",
+      },
+
+      name: {
+        type: String,
+        required: "Name of exercise is required",
+      },
+
+      duration: {
+        type: Number,
+        required: "Duration is required",
+      },
+
+      weight: {
+        type: Number,
+      },
+
+      reps: {
+        type: Number,
+      },
+
+      sets: {
+        type: Number,
+      },
+
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const workout = mongoose.model("workout", workoutSchema);
+
+module.exports = workout;
